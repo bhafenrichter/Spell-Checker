88ec7dad469ded88a0b492ccdcbacdb29b20aef5
Brandon Hafenrichter <bhafenri@gmail.com>
2015-10-05 00:19:01 -0500

    Added Spell Check Traversal and Entries Display

--- src/spellchecker/SpellChecker.java
+++ src/spellchecker/SpellChecker.java
@@ -1,13 +1,17 @@
 package spellchecker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SpellChecker {
 
     public static Trie trie;
-
+    public static ArrayList<String> entries;
+    
     public static void main(String[] args) {
         trie = new Trie();
+        entries = new ArrayList<String>();
         getUserAction();
     }
 
@@ -46,7 +50,9 @@ public class SpellChecker {
         for (int i = 0; i < split.length; i++) {
             String cur = split[i];
             TraverseTree(trie.root, cur, 0);
+            entries.add(cur);
         }
+        getUserAction();
     }
 
     public static void TraverseTree(Node n, String word, int index) {
@@ -66,26 +72,91 @@ public class SpellChecker {
         } else {
             //traverse the children and see if the current letter is one of them
             char wordData = word.charAt(index);
+            boolean hasTraversed = false;
             for (int i = 0; i < n.children.size(); i++) {
                 char nodeData = n.children.get(i).letter;
                 //child exists, traverse to the next letter
                 if (nodeData == wordData) {
-                    TraverseTree(n.children.get(i), word, index++);
+                    hasTraversed = true;
+                    TraverseTree(n.children.get(i), word, ++index);
                 }
             }
-            //child doesn't exist, add node to parent and traverse down it
-            n.children.add(new Node(wordData));
-            TraverseTree(n.children.get(n.children.size() - 1), word, index++);
+            
+            //there haven't been any matches, therefore create a new child node 
+            if (hasTraversed == false) {
+                //child doesn't exist, add node to parent and traverse down it
+                n.children.add(new Node(wordData));
+                TraverseTree(n.children.get(n.children.size() - 1), word, ++index);
+            }
+            
         }
     }
 
     private static void SpellCheckFile() {
         String[] text = GetTextFromFile();
+        String[] split = text[0].split(" ");
 
+        ArrayList<String> incorrectWords = new ArrayList<String>();
+        for (int i = 0; i < split.length; i++) {
+            String word = split[i];
+            boolean isIncorrect = false;
+            //this node is the "!" node, that extends to the actual nodes in the tree
+            Node root = trie.root;
+            for (int j = 0; j < word.length() + 1; j++) {
+                //we've reached the end of the word, see if the last node contains stop value
+                if(j == word.length()){
+                    //flag to see whether word has stop value
+                    boolean isComplete = false;
+                    for (int k = 0; k < root.children.size(); k++) {
+                        char cur = root.children.get(k).letter;
+                        if(cur == '#'){
+                            isComplete = true;
+                        }
+                    }
+                    if(isComplete){
+                        break;
+                    }else{
+                        isIncorrect = true;
+                        break;
+                    }
+                }
+                char cur = word.charAt(j);
+                //set to true if current letter is in the nodes children
+                boolean containsLetter = false;
+                //check to see if cur is a child of current node
+                for (int k = 0; k < root.children.size(); k++) {
+                    if(root.children.get(k).letter == cur){
+                        containsLetter = true;
+                        root = root.children.get(k);
+                    }
+                }
+                
+                //letter is not in tree, this word is spelled incorrectly
+                if(!containsLetter){
+                    isIncorrect = true;
+                }
+            }
+            
+            //if the word is incorrect, add it to the list of incorrect words
+            if(isIncorrect){
+                incorrectWords.add(word);
+            }
+        }
+        
+        //Prints out all of the incorrect words 
+        if(incorrectWords.size() == 0){
+            System.out.println("All words are spelled correctly.");
+        }else{
+            System.out.println("The following words are spelled incorrectly: " + Arrays.toString(incorrectWords.toArray()));
+        }
+        
+        getUserAction();
     }
 
     private static void ShowEntries() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Collections.sort(entries);
+        System.out.println("Entries in Dictionary: " + entries.toString());
+        getUserAction();
     }
 
     public static String[] GetTextFromFile() {
